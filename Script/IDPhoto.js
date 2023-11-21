050d8af86ebd24f48fe5c7d59c5f97ca6b0332b4
Cullwen <99164208+Cullwen@users.noreply.github.com>
2023-11-21 13:39:35 +0800

    Update IDPhoto.js

--- Script/IDPhoto.js
+++ Script/IDPhoto.js
@@ -15,8 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/standard\.rhinoxlab\.com\/standard\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/zjzXg.js
-
+^https:\/\/standard\.rhinoxlab\.com\/standard\/account\/getAccountInfo url script-response-body https://raw.githubusercontent.com/Cullwen/Quantumult/main/Script/IDPhoto.js
 [mitm] 
 
 hostname = standard.rhinoxlab.com
